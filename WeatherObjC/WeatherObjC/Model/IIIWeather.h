d080bfe2b6cfeedca51e36076c0774cca73c66af
Nar Kumar <njn.kumar@gmail.com>
2020-01-21 17:05:47 -0800

    Initial Commit

---
+++ WeatherObjC/WeatherObjC/Model/IIIWeather.h
@@ -0,0 +1,24 @@
+//
+//  IIIWeather.h
+//  WeatherObjC
+//
+//  Created by Niranjan Kumar on 1/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IIIWeather : NSObject
+
+@property NSString *city;
+@property NSNumber *number;
+@property UIImage *image;
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WeatherObjC/WeatherObjC/Model/IIIWeather.m
@@ -0,0 +1,13 @@
+//
+//  IIIWeather.m
+//  WeatherObjC
+//
+//  Created by Niranjan Kumar on 1/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "IIIWeather.h"
+
+@implementation IIIWeather
+
+@end
